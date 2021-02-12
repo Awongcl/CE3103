487e2c3ad469a08dd5b901e99aaa53c4c7d3d9c3
awongcl <awongcl30@gmail.com>
2021-02-13 01:16:26 +0800

    Updatae

---
+++ Lab_2/client.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <strings.h>
+#include <string.h>
+
+// Error message
+void error(char *m){
+	perror(m);
+	exit(0);
+}
+
+int main(int argc, char *argv[]){
+	int sockfd, port, n;
+	char buffer[256];
+	struct sockaddr_in serv_addr;
+	struct hostent *server;   // Host entity
+	
+	if (argc < 3)    // [hostname] [port] 
+		error("Error, no port provided\n");
+	// Convert stringto interger
+	port = atoi(argv[2]);
+	//printf("%i\n",port);
+	// socket returns a non-negative value if success
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0)
+		error("Error opening socket");
+	server = gethostbyname(argv[1]);
+	if (server == NULL)
+		error("Error, no such host\n");
+	// Start with a clean address structure
+	memset((char *) &serv_addr, 0, sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET; // Internet socket
+	bcopy ((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(port); //  host to network
+	// htons converts unsigned short integer to network byte order
+	// Returns -1 id binding fails
+	if (connect(sockfd, &serv_addr, sizeof(serv_addr)) < 0)
+		error("Error connecting to server\n");
+	printf("Please enter the message\n");
+	fgets(buffer,255,stdin);
+	// Send data to server
+	n = write(sockfd, buffer, strlen(buffer));
+	if(n < 0)  // returns 0 if success
+		error("Error writing to server\n");
+	n = read(sockfd, buffer, 255);
+	if(n < 0)
+		error("Error reading from server\n");
+	int s = ntohs(buffer);
+	printf("Result after multiplying by 5: %d\n",s);
+	return 0;
+}
--- Lab_2/server.c
+++ Lab_2/server.c
@@ -3,29 +3,58 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <strings.h>
+#include <string.h>
 
+// Error message
 void error(char *m){
 	perror(m);
 }
 
 int main(int argc, char *argv[]){
-	int sockfd, newsockfd, port, client,  n;
+	int sockfd, newsockfd, port, clilen, n;
 	char buffer[256];
-	struct sockaddr_in serv_addr, client_addr;
+	struct sockaddr_in serv_addr, cli_addr;
 	if (argc < 2)
 		error("Error, no port provided\n");
+	// Convert stringto interger
 	port = atoi(argv[1]);
+	// socket returns a non-negative value if success
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	int enable = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+    		error("setsockopt(SO_REUSEADDR) failed");
 	if(sockfd < 0)
 		error("Error opening socket");
-
+	// Copy 0's to address of serv_address 
 	memset((char *) &serv_addr, 0, sizeof(serv_addr));
-
+	//  
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	// INADDR_ANY binds to all available interfaces
+	serv_addr.sin_addr.s_addr = INADDR_ANY;  //struct sin_addr -> unsigned long s_addr
 	serv_addr.sin_port = htons(port); //  host to network
-
+	// htons converts unsigned short integer to network byte order
+	// Returns -1 id binding fails
 	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("Error binding to socket\n");
-	printf("Server is listening");
+	printf("Server is listening on port %i \n",port);
+	listen(sockfd,2); // Backlog = 2, maax length of queue of pending connections
+	clilen = sizeof(cli_addr);
+
+	newsockfd = accept(sockfd,(struct sockaddr*) &cli_addr, &clilen);
+	if (newsockfd < 0)
+		error("Error on accept");
+	n = read(newsockfd, buffer, 255);
+	if (n < 0)
+		error("Error reading from socket\n");
+	printf("Message received: %s\n", buffer);
+	int result = atoi(buffer);
+	result =  result*5;    
+	result = htons(result);
+	printf("%d\n",ntohs(result));
+	//printf(&result);
+	n = write(newsockfd,&result,sizeof(result));
+	if(n<0)
+		error("Error writing back to socket\n");
+
+	return 0;
 }
